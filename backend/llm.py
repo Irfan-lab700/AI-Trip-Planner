14e2b9d0198fcae36c5486d588315ed905ad5b16
Irfan khan <irfankhan016104@gmail.com>
2026-02-22 10:26:26 +0530

    Removal

--- backend/llm.py
+++ backend/llm.py
@@ -1,9 +1,10 @@
+import os
 from huggingface_hub import InferenceClient
 client = InferenceClient(
-    model="Qwen/Qwen2.5-7B-Instruct",
-    token="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"
+    model = "Qwen/Qwen2.5-7B-Instruct",
+    token=os.getenv("HF_TOKEN")
 )
-def generate_itinerary(user_data):
+def generate_output(user_data):
     prompt = f"""
     Generate a {user_data['day']}-day itinerary for a trip from {user_data['source']} 
     to {user_data['destination']}. Interests: {', '.join(user_data['interests'])}. 
@@ -11,14 +12,14 @@ def generate_itinerary(user_data):
     Return the result in a simple list for each day.
     """
     response = client.chat.completions.create(
-        messages=[{"role": "user", "content": prompt}],
-        max_tokens=500
+        messages = [{"role":"user", "content": prompt,}],
+        max_tokens = 500
     )
     ai_text = response.choices[0].message.content
     return ai_text
 if __name__ == "__main__":
-    from user_input import get_user_input   # import your user input function
-
-    user_data = get_user_input()            # get dict from user
-    itinerary = generate_itinerary(user_data)  # call LLM function
+    from user_input import get_user_input 
+    user_data = get_user_input() 
+    itinerary = generate_output(user_data) 
     print("\nAI-generated Itinerary:\n", itinerary)
+    
